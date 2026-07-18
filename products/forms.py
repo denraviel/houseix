4caa16c8b6a7d91107b4e6120b97a9f73628696b
Damilola Tytler <79582603+denraviel@users.noreply.github.com>
2026-07-18 09:09:50 +0100

    Update products/forms.py

--- products/forms.py
+++ products/forms.py
@@ -6,4 +6,28 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['name', 'category', 'selling_price', 'cost_price', 'quantity_in_stock', 'unit_type', 'is_available']
-
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field.widget, forms.widgets.Input):
+                field.widget.attrs['class'] = 'form-control'
+            elif isinstance(field.widget, forms.widgets.Select):
+                field.widget.attrs['class'] = 'form-select'
+            elif isinstance(field.widget, forms.widgets.CheckboxInput):
+                field.widget.attrs['class'] = 'form-check-input'
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        name = cleaned_data.get('name')
+        category = cleaned_data.get('category')
+        
+        # Check for duplicates, exclude current instance if editing
+        queryset = Product.objects.filter(name=name, category=category)
+        if self.instance.pk:
+            queryset = queryset.exclude(pk=self.instance.pk)
+        
+        if queryset.exists():
+            raise forms.ValidationError("Product already exists. Please edit the existing product instead.")
+        
+        return cleaned_data
