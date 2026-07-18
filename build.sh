fe0d3dbcd0e756e35abf0567c2b83efa2260ee99
Damilola Tytler <79582603+denraviel@users.noreply.github.com>
2026-07-18 08:59:20 +0100

    Add build.sh for Render deployment

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -o errexit
+
+echo "Installing dependencies..."
+pip install -r requirements.txt
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+echo "Running database migrations..."
+python manage.py migrate --noinput
